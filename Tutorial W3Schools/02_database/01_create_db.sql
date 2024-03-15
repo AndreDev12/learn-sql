9ec150c2432813dece25aa4239bdd704b05e9d91
André Torres <andretorresjava@gmail.com>
2024-03-15 11:33:21 -0500

    Sentencias create database y drop database. (W3S)

---
+++ Tutorial W3Schools/02_database/01_create_db.sql
@@ -0,0 +1,7 @@
+-- La declaración CREATE DATABASE se utiliza para crear una nueva base de datos SQL
+
+-- Crea una base de datos llamada "testDB"
+CREATE DATABASE testDB;
+
+-- Una vez creada una base de datos, puede consultarla en la lista de bases de datos con el siguiente comando SQL
+-- SHOW DATABASES;
---
+++ Tutorial W3Schools/02_database/02_drop_db.sql
@@ -0,0 +1,4 @@
+-- La declaración DROP DATABASE se utiliza para descartar una base de datos SQL existente
+
+-- Elimina la base de datos existente "testDB"
+DROP DATABASE testDB;
